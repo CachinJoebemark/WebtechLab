d2f04b71d6339c27af85fcb4e074241e429fc79a
rox65 <34298121+rox65@users.noreply.github.com>
2018-04-17 21:01:54 +0800

    Update function.js
    added summation for rendered services

--- PWA/js/function.js
+++ PWA/js/function.js
@@ -110,5 +110,16 @@ function showAll(){
 		document.getElementById('storageOut').innerHTML = result;
 	}
 }
+//para jay summation ti amount for rendered services (ijay checklist)
+function checkTotal() {
+		document.listForm.total.value = '';
+		var sum = 0;
+		for (i=0;i<document.listForm.choice.length;i++) {
+		  if (document.listForm.choice[i].checked) {
+		  	sum = sum + parseInt(document.listForm.choice[i].value);
+		  }
+		}
+		document.listForm.total.value = sum;
+	}
 
 
