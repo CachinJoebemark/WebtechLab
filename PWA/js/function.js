e523b47d2d2555a78a8e3fa19eca68bd5d02a50c
ArnelArnel <34260271+ArnelArnel@users.noreply.github.com>
2018-04-17 18:35:53 +0800

    Made all function work

--- PWA/js/function.js
+++ PWA/js/function.js
@@ -9,12 +9,16 @@ function set() {
 	nameFirst.push(first);
 	localStorage.setItem('name',JSON.stringify(nameFirst));
 	//Date
-	//todo
-	/*Icomplete
 	var date = !!localStorage.getItem('date') ? JSON.parse(localStorage.getItem('date')): [];
 	var d = new Date();
 	var s = d.toLocaleDateString();
-	*/
+	date.push(s);
+	localStorage.setItem('date',JSON.stringify(date));
+
+		
+	//Amount/Price
+		//todo
+	
 	} else {  
    	document.write('browser not supported');
 	}  
@@ -23,14 +27,55 @@ function set() {
 	var eventhehe = document.getElementById('insertData');
 	eventhehe.addEventListener('click', function() {
      set();
+     getValue();
      getPrice();
 	});
+	
 
-	//Services
-	//todo
-	/*Incomplete
-	*/
+	/*var eventsum = document.getElementById('getSum');
+	eventsum.addEventListener('click',function(){
+		getPrice();
+	});*/
 
+//E save na jay value ti checkbox nga ada check na hahaha
+/*function addService(){
+	var serArr = !!localStorage.getItem('str') ? JSON.parse(localStorage.getItem('str')): [];
+	var checkedValue = null; 
+	var inputElements = document.getElementsByClassName('messageCheckbox');
+	var tempVal = null;
+	for(var i=0; i < 10; i++){
+      	if(inputElements[i].checked === true){
+	        checkedValue = inputElements[i].name;
+	        serArr.push(checkedValue);      
+	    }
+	    
+	    localStorage.setItem('str',JSON.stringify(serArr));
+	}	
+}
+*/
+
+function showConfirm(){
+	var r=confirm("Are you sure you want to clear the storage? Please be reminded that this may not be undone.")
+		if(r==true){
+			alert("The storage has been cleared!")
+			clearStorage();
+		}
+		else{
+			alert("You chose not to clear the storage.")
+		}
+}
+
+function getValue() {
+	var myArray = !!localStorage.getItem('service') ? JSON.parse(localStorage.getItem('service')) : [];
+ 	var checks = document.querySelectorAll('.messageCheckbox:checked');
+  	var myChecks = [];
+  	if (!checks.length) return;
+		for (i = 0; i < checks.length; i++) {
+		  myChecks.push(checks[i].name);
+		}
+		  myArray.push(myChecks.join(','));  
+		  localStorage.setItem('service', JSON.stringify(myArray));
+}
 /*
 */
 //Alaen na jay total boi ikarga na iti array nga agnagan ti sumArr
@@ -46,33 +91,22 @@ function getPrice(){
 	}
 	localStorage.setItem('price',JSON.stringify(summer));
 }
-//Clears localStorage
+
 function clearStorage(){
 		window.localStorage.clear();
 		location.reload();
 }
-//Add pop up function here - Julius
-function showConfirm(){
-	var r=confirm("Are you sure you want to clear the storage? Please be reminded that this may not be undone.")
-		if(r==true){
-			alert("The storage has been cleared!")
-			clearStorage();
-		}
-		else{
-			alert("You chose not to clear the storage.")
-		}
-}
-//
+
 //Ipakita na amin lool
 function showAll(){
 	for(let i = 0;i < localStorage.length;i++){
 		var fullname = localStorage.getItem('name');
-		//var last = localStorage.getItem('last');
-		//var today = localStorage.getItem('date');
+		var last = localStorage.getItem('last');
+		var today = localStorage.getItem('date');
 		var service = localStorage.getItem('service');
-		//var price = localStorage.getItem('price')
+		var price = localStorage.getItem('price')
 
-		var result = "Patient Name:"+fullname +" "+service;
+		var result = "Patient Name:"+fullname +"<br/> Date: "+ today +"<br/> Service(s): "+service+"<br/> Price: "+price;
 		document.getElementById('storageOut').innerHTML = result;
 	}
 }
